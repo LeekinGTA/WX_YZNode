6497602acf78d20f2f1c4eddf208a79c9b2bacb3
wu2LearnTeam <liyuechun2009@163.com>
2016-09-29 23:34:02 +0800

    rn_demo

---
+++ rn_YZNode/HomePage/Cell.js
@@ -0,0 +1,50 @@
+/**
+ * Created by lucien on 16/9/24.
+ */
+
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    StyleSheet
+} from 'react-native';
+
+class Cell extends Component {
+
+    render() {
+
+        return (
+            <View style={{backgroundColor: 'white',height: 60,marginTop: 15}}>
+                <Text style={{
+                    fontSize:16,
+                    color: 'rgb(83,83,83)',
+                    flex: 1,
+                    marginLeft: 5
+                }}>{this.props.rowDic.title}</Text>
+                <View style={{
+                    flexDirection: 'row',
+                    height: 25,
+                    backgroundColor: 'white',
+                    alignItems: 'center'}}>
+                    <Text style={[styles.base, {color: 'rgb(169,212,99)'}]}>{this.props.rowDic.tab}</Text>
+                    <Text style={[styles.base, {color: 'rgb(142,142,142)'}]}>
+                        {this.props.rowDic.author.loginname}|{this.props.rowDic.reply_count}回复|{this.props.rowDic.visit_count}阅读
+                    </Text>
+                </View>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+
+    base: {
+        fontSize: 12,
+        margin: 5
+    }
+});
+
+
+
+
+export default Cell;
---
+++ rn_YZNode/HomePage/HomeView.js
@@ -0,0 +1,93 @@
+/**
+ * Created by lucien on 16/9/24.
+ */
+
+
+import React, {Component} from 'react';
+import {
+    View,
+    Text,
+    ListView
+} from  'react-native';
+
+
+import Cell from './Cell';
+
+import Api from '../Api';
+
+class HomeView extends Component {
+
+    // 构造
+    constructor(props) {
+        super(props);
+        // 初始状态
+        this.state = {
+            isFirst: false,
+            dataSource: new ListView.DataSource({rowHasChanged: (row1, row2) => row1 !== row2})
+        };
+    }
+
+    render() {
+
+        if (!this.state.isFirst) {
+            return (
+                <Text style={{marginTop: 80, textAlign: 'center'}}>正在刷新...</Text>
+            )
+        }
+
+        return (
+            <ListView
+                style={{backgroundColor: 'rgb(236,236,236)', marginTop: 64}}
+                dataSource={this.state.dataSource}
+                renderRow={(rowData) => this.renderRow(rowData)}
+            />
+        )
+
+    }
+
+    //组件加载完成,从这里面下载数据
+    componentDidMount() {
+
+        this.getData();
+    }
+
+
+    getData() {
+
+        console.log(Api.homePage);
+        fetch(Api.homePage)
+            .then((data) => data.json())
+            .then((dataDic) => {
+                console.log(dataDic);
+                var dataArr = dataDic.data;
+                if (dataArr) {
+                    this.setState({
+                        dataSource: this.state.dataSource.cloneWithRows(dataArr),
+                        isFirst: true
+                    });
+                }
+            }).done();
+
+
+    }
+
+    //自定义cell
+    renderRow(rowData) {
+
+        return (
+            <Cell rowDic={rowData} />
+        )
+    }
+}
+
+export default HomeView;
+
+
+
+
+
+
+
+
+
+
